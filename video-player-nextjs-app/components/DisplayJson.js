c0147fad78086db86ddcda7405c3bc255c415139
Ruihao_Zhang <zhangrh16@mails.thu.edu.cn>
2024-04-15 21:25:24 -0400

    setJson in page

--- video-player-nextjs-app/components/DisplayJson.js
+++ video-player-nextjs-app/components/DisplayJson.js
@@ -1,25 +1,6 @@
 // components/DisplayJson.js
-import React, { useEffect, useState } from 'react';
 import TranscriptionsDisplay from './TranscriptionsDisplay';
-const DisplayJson = ({ jsonUrl }) => {
-  const [jsonData, setJsonData] = useState({});
-
-  useEffect(() => {
-    const fetchJsonData = async () => {
-      try {
-        const response = await fetch(jsonUrl);
-        const data = await response.json();
-        setJsonData(data);
-      } catch (error) {
-        console.error('Error fetching JSON:', error);
-      }
-    };
-
-    if (jsonUrl) {
-      fetchJsonData();
-    }
-  }, [jsonUrl]);
-
+const DisplayJson = ({ jsonData }) => {
   return (
     // <pre>{JSON.stringify(jsonData, null, 2)}</pre>
     <TranscriptionsDisplay jsonData={jsonData} />
--- video-player-nextjs-app/pages/[uuid].js
+++ video-player-nextjs-app/pages/[uuid].js
@@ -8,7 +8,8 @@ const VideoPage = () => {
   const router = useRouter();
   const { uuid } = router.query;
   const [videoUrl, setVideoUrl] = useState('');
-  const [jsonUrl, setJsonUrl] = useState({});
+  const [jsonUrl, setJsonUrl] = useState('');
+  const [jsonData, setJsonData] = useState(null);
 
   useEffect(() => {
     if (!uuid) return;
@@ -16,25 +17,30 @@ const VideoPage = () => {
     // Fetch API 
     const fetchData = async () => {
       try {
-        // Fetch video file uri and json file uri
+        // Fetch video file uri and json file uri based on uuid
         const response = await fetch(`/api/video/${uuid}`);
-        console.log("searching for video", uuid)
-        console.log("response", response)
         const data = await response.json();
         setVideoUrl(data.videoUrl);
         setJsonUrl(data.jsonUrl);
-        console.log("fetch data", data)
+        
+        // Fetch JSON data if a jsonUrl is provided
+        if (data.jsonUrl) {
+          const jsonResponse = await fetch(data.jsonUrl);
+          const jsonData = await jsonResponse.json();
+          setJsonData(jsonData);
+        }
       } catch (error) {
         console.error('Failed to fetch data:', error);
       }
     };
+
     fetchData();
-  }, [uuid]);
+  }, [uuid]); // Dependency on uuid ensures this effect runs only when uuid changes
 
   return (
     <div>
       {videoUrl && <VideoPlayer src={videoUrl} />}
-      {jsonUrl && <DisplayJson jsonUrl={jsonUrl} />}
+      {jsonData && <DisplayJson jsonData={jsonData} />}
     </div>
   );
 };
